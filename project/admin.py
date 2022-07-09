8f5c17866db86eb2605b50b966f47204549913e4
Muerdis <a_shulaev@indatalabs.com>
2022-07-09 22:39:47 +0300

    Add tokens export

--- project/admin.py
+++ project/admin.py
@@ -1,6 +1,44 @@
+import csv
+
 from django.contrib import admin
+from django.http import HttpResponse
 
 from project.models import Token, Blockchain
 
+
+def download_csv(modeladmin, request, queryset):
+    response = HttpResponse(content_type="text/csv")
+    response["Content-Disposition"] = "attachment; filename=tokens.csv"
+
+    writer = csv.writer(response)
+
+    fields = [
+        "created", "modified"
+        "name", "score", "blockchain", "category", "audit", "twitter",
+        "website", "audit_date", "is_partner", "contract_address"
+    ]
+
+    writer.writerow(fields)
+    for item in queryset:
+        data = []
+        for field in fields:
+            if field == "blockchain":
+                blockchain = getattr(item, field, None)
+                if blockchain:
+                    data.append(getattr(blockchain, "name"))
+                else:
+                    data.append("")
+            else:
+                data.append(getattr(item, field, ""))
+
+        writer.writerow(data)
+
+    return response
+
+
+class TokenAdmin(admin.ModelAdmin):
+    actions = [download_csv]
+
+
 admin.site.register(Blockchain)
-admin.site.register(Token)
+admin.site.register(Token, TokenAdmin)
