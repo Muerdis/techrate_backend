e9407d30eef1f4a8c31fe36dd1e4d108ac44cee3
Aleksandr Shulaev <a_shulaev@indatalabs.com>
2022-09-20 12:01:34 +0300

    Update image for Blockchain serializer

--- api/project/serializers/blockchain.py
+++ api/project/serializers/blockchain.py
@@ -10,7 +10,12 @@ class BlockchainSerializer(serializers.ModelSerializer):
     """
     Blockchain serializer
     """
+    image = serializers.SerializerMethodField(help_text="Image")
 
     class Meta:
         model = Blockchain
         fields = "__all__"
+
+    @staticmethod
+    def get_image(obj):
+        return f"https://techrate.org{obj.image.url}" if obj.image else None
