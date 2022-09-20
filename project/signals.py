fd4d139041a92620fd8fc4cdef714e5de9fada3a
Aleksandr Shulaev <a_shulaev@indatalabs.com>
2022-09-20 11:30:54 +0300

    Minor update for token indexes

--- project/signals.py
+++ project/signals.py
@@ -2,20 +2,30 @@
 Project signals
 """
 from django.db.models import F
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
 from project.models import Token
 
 
 @receiver(post_save, sender=Token)
-def add_index_for_token(sender, instance, created, **kwargs):  # pylint: disable=unused-argument
+def add_indexes_for_tokens(sender, instance, created, **kwargs):  # pylint: disable=unused-argument
     """
     Add index for token
     """
     if not created:
         return
 
+    tokens = Token.objects.all()
+    index = 0
+    for token in tokens:
+        index += 1
+        token.index = index
+        token.save()
+
+
+@receiver(post_delete, sender=Token)
+def update_indexes_for_tokens(sender, instance, *args, **kwargs):
     tokens = Token.objects.all()
     index = 0
     for token in tokens:
